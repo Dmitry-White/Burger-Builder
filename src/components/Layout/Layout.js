c7fbccd6027730feb50f01e0ca63b9e09d9a660c
Dmitry <ecio1476@gmail.com>
2018-02-18 12:02:33 +0800

    Add basic toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import classes from './Layout.module.css'
 
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+
 const layout = (props) => (
     <>
-        <div> Toolbar, SideDrawer</div>
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
