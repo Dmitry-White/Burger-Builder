63421b92b99d40f6fdf7dd332134ba4ea75c9ffc
Dmitry <ecio1476@gmail.com>
2018-03-08 14:55:20 +0800

    Complete Orders component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+
 class App extends Component {
   render() {
     return (
--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './NavigationItem.module.css';
 
 const navigationItem = props => (
     <li className={classes.NavigationItem}>
-        <a 
-            className={props.active ? classes.active : null}
-            href={props.link}
-        >{props.children}</a>
+        <NavLink
+            exact
+            to={props.link}
+            activeClassName={classes.active}
+        >{props.children}</NavLink>
     </li>
 );
 
