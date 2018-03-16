bfc88a9c4e0675de25c7f42f2147400b922a5d5a
Dmitry <ecio1476@gmail.com>
2018-03-16 17:53:34 +0800

    Add Redux Dev Tools variable

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ import reducer from './store/reducer';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const app = (
     <Provider store={store}>
