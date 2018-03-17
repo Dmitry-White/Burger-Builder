2661d23703bca96ee47b0cc420fcf08ebccee59c
Dmitry <ecio1476@gmail.com>
2018-03-17 17:26:18 +0800

    Solve ings=null problem

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
@@ -17,19 +17,23 @@ class Checkout extends Component {
     };
 
     render() {
-        return (
-            <div>
-                <CheckoutSummary 
-                    ingredients={this.props.ings}
-                    checkoutCancelled={this.checkoutCancelledHandeler}
-                    checkoutContinued={this.checkoutContinuedHandeler}
-                />
-                <Route 
-                    path={this.props.match.path + '/contact-data'}
-                    component={ContactData}
-                />
-            </div>
-        )
+        let summary = <Redirect to="/"/>;
+        if (this.props.ings) {
+            summary = (
+                <div>
+                    <CheckoutSummary 
+                        ingredients={this.props.ings}
+                        checkoutCancelled={this.checkoutCancelledHandeler}
+                        checkoutContinued={this.checkoutContinuedHandeler}
+                    />
+                    <Route 
+                        path={this.props.match.path + '/contact-data'}
+                        component={ContactData}
+                    />
+                </div>
+            );
+        };
+        return {summary};
     }
 };
 
