6f78746b617c9e6e8164dde89cd6df21e4545d9b
Dmitry <ecio1476@gmail.com>
2018-03-22 10:54:07 +0800

    Add guards to routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from "react-router-dom";
+import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout'
@@ -16,20 +16,40 @@ class App extends Component {
   };
 
   render() {
+    let route = (
+      <Switch>
+        <Route path="/auth" component={Auth}/>
+        <Route path="/" component={BurgerBuilder}/>
+        <Redirect to="/"/>
+      </Switch>
+    );
+
+    if(this.props.isAuth) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout}/>
+          <Route path="/orders" component={Orders}/>
+          <Route path="/logout" component={Logout}/>
+          <Route path="/" component={BurgerBuilder}/>
+          <Redirect to="/"/>
+        </Switch>
+      );
+    };
+
     return (
       <>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout}/>
-            <Route path="/orders" component={Orders}/>
-            <Route path="/auth" component={Auth}/>
-            <Route path="/logout" component={Logout}/>
-            <Route path="/" component={BurgerBuilder}/>
-          </Switch>
+          {routes}
         </Layout>
       </>
     );
   };
+};
+
+const mapStateToProps = state => {
+  return {
+    isAuth: state.auth.idToken !== null
+  }
 };
 
 const mapDispatchToProps = dispatch => {
@@ -38,4 +58,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
