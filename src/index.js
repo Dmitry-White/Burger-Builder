083c3a9367e5cb571b6e766028e9884f5ed70437
Dmitry <ecio1476@gmail.com>
2018-04-29 21:30:44 +0800

    Fix redux dev tools bug

--- src/index.js
+++ src/index.js
@@ -20,7 +20,9 @@ const rootReducer = combineReducers({
     auth: authReducer
 });
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' ? (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose) : null || compose;
+
+console.log(null);
 
 const sagaMiddleware = createSagaMiddleware();
 
